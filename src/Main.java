e98805bb5ecce6314617f0254d2ce8bf6cf550de
unknown <erinneassing1@gmail.com>
2025-11-17 14:56:48 +0400

    Modification du message de Main (version B)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 
 public class Main {
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Projet collaboratif - TP2 (version B)");
-		}
+	}
 }
-
-
